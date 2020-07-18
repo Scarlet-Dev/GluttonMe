11ad9281706ff3a36b12b83a895d18ea03b7acfc
Scarlet <developerscarlet@gmail.com>
2020-07-17 22:44:18 -0400

    Create ProviderRepo

---
+++ GluttonMe.Api/Repository/ProviderRepository.cs
@@ -0,0 +1,47 @@
+﻿using GluttonMe.Api.Interfaces;
+using GluttonMe.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Repository
+{
+    public class ProviderRepository : IProviderRepository<Provider>
+    {
+        public ProviderRepository()
+        {
+
+        }
+
+        public Task<Provider> create(Provider model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Provider> delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Provider> get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Provider> get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Provider> searchOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Provider> update(int id, Provider model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
