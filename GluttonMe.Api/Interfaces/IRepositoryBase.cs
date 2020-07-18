9ac980cfa96272b8f6dbcf5dc8a70956e5795999
Scarlet <developerscarlet@gmail.com>
2020-07-17 21:57:30 -0400

    Create IRepo base

---
+++ GluttonMe.Api/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Interfaces
+{
+    public interface IRepositoryBase<F>
+    {
+        public Task<F> get();
+
+        public Task<F> get(int id);
+
+        public Task<F> create(F model);
+
+        public Task<F> update(int id, F model);
+
+        public Task<F> delete(int id);
+    }
+}
