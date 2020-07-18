ae462fe88c0dd234fd0ec0d374bbf117a17d7a91
Scarlet <developerscarlet@gmail.com>
2020-07-17 22:44:06 -0400

    Create IProviderRepo; WIP

---
+++ GluttonMe.Api/Interfaces/IProviderRepository.cs
@@ -0,0 +1,13 @@
+﻿using GluttonMe.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Interfaces
+{
+    public interface IProviderRepository<P> : IRepositoryBase<P>
+    {
+        public IList<P> searchOrder(int id);
+    }
+}
