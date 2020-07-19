f73f392639832cf2ff00bf2910eed4a634b2136c
Scarlet <developerscarlet@gmail.com>
2020-07-18 22:08:38 -0400

    Add DatabaseInitializer class

---
+++ GluttonMe.Api/GluttonMeDatabaseInitializer.cs
@@ -0,0 +1,32 @@
+﻿using GluttonMe.Api.Interfaces;
+using GluttonMe.Api.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api
+{
+    public class GluttonMeDatabaseInitializer
+    {
+        private MongoClient _mainClient;
+        public static IMongoDatabase _mainDatabase;
+
+        private GluttonMeDatabaseInitializer(IGluttonMeDatabaseSettings settings)
+        {
+            _mainClient = new MongoClient(settings.ConnectionString);
+            _mainDatabase = _mainClient.GetDatabase(settings.DatabaseName);
+        }
+
+        private static readonly GluttonMeDatabaseInitializer _instance =
+            new GluttonMeDatabaseInitializer(new GluttonMeDatabaseSettings());
+
+        public static GluttonMeDatabaseInitializer Instance => _instance;
+
+        public IMongoDatabase GetMongoDatabase()
+        {
+            return _mainDatabase;
+        }
+    }
+}
