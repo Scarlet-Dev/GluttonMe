997d84408d545b35bb3203a204a11a7e783eb1e0
Scarlet <developerscarlet@gmail.com>
2020-07-19 18:36:27 -0400

    Add GluttonMeDbSettings model

---
+++ GluttonMe.Api/Models/GluttonMeDatabaseSettings.cs
@@ -0,0 +1,19 @@
+﻿using GluttonMe.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Models
+{
+    public class GluttonMeDatabaseSettings : IGluttonMeDatabaseSettings
+    {
+        public GluttonMeDatabaseSettings()
+        {
+        }
+
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+        public string[] CollectionNames { get; set; }
+    }
+}
