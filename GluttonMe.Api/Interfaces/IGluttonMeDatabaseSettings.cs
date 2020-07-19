549f82f99cf50f01c1e7b8ac08379e0c4c1d57c5
Scarlet <developerscarlet@gmail.com>
2020-07-18 22:13:36 -0400

    Change CollectionName to CollectionNames

---
+++ GluttonMe.Api/Interfaces/IGluttonMeDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Interfaces
+{
+    public interface IGluttonMeDatabaseSettings
+    {
+        string DatabaseName { get; set; }
+        string[] CollectionNames { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
