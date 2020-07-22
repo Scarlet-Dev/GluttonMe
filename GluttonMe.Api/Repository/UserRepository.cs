3defb72e5adc9fb2eef0c164e8e9dc81695ca001
Scarlet <developerscarlet@gmail.com>
2020-07-21 22:45:28 -0400

    Add UserRepository

---
+++ GluttonMe.Api/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using GluttonMe.Api.Interfaces;
+using GluttonMe.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Repository
+{
+    public class UserRepository : IUserRepository<User>
+    {
+        public User Create(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, User model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
