0c14dcbe9ac4da5cc36b592cb7dc16f84972ea58
Scarlet <developerscarlet@gmail.com>
2020-07-21 22:45:02 -0400

    Add OrderRepository

---
+++ GluttonMe.Api/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Interfaces
+{
+    public interface IOrderRepository<O>: IRepositoryBase<O>
+    {
+    }
+}
---
+++ GluttonMe.Api/Repository/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using GluttonMe.Api.Interfaces;
+using GluttonMe.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Repository
+{
+    public class OrderRepository : IOrderRepository<Order>
+    {
+        public Order Create(Order model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Order model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Order model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
