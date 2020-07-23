b991c150b719c350c58434e0ebe22524472c6c44
Scarlet <developerscarlet@gmail.com>
2020-07-22 20:54:14 -0400

    Add InvoiceRepository

---
+++ GluttonMe.Api/Repository/InvoiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using GluttonMe.Api.Interfaces;
+using GluttonMe.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GluttonMe.Api.Repository
+{
+    public class InvoiceRepository:IInvoiceRepository<Invoice>
+    {
+    }
+}
